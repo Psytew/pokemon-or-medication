39c7083dd230f1a9ab6d4ee9cab748c1edca21af
Patrick <patrickwestervelt.us@gmail.com>
2019-09-04 17:24:15 -0400

    Trying to fix heroku version of app

--- app/Http/Controllers/AuthenticatedRouteController.php
+++ app/Http/Controllers/AuthenticatedRouteController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\user;
+use App\User;
 use Illuminate\Http\Request;
 
 class AuthenticatedRouteController extends Controller
--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\user;
+use App\User;
 use Illuminate\Http\Request;
 
 class RouteController extends Controller
